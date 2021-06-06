f5c5dda4f10041cc34c3e16ba72bf9627fa612dd
Mati2108 <caloperaltaramosm@udesa.edu.ar>
2021-06-06 12:14:52 -0300

    Mouse Arriba y un ppoco de java script

---
+++ script/detailsong.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', function(){
+
+let clickheart = document.querySelector('.heart')
+
+let clickplay = document.querySelector("#play")
+
+let clickHeWhi = document.querySelector(".heartConClick")
+
+clickheart.addEventListener('click', function () {
+    clickheart.classList.toggle(".heartConClick")
+})
+
+
+
+
+})
+
