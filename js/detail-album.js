f3157cc5d22c326635d949657ea33c4792a26133
naranujo <naraujo@udesa.edu.ar>
2021-06-07 18:39:46 -0300

    corrección det.album.html y agrego det.album.js

---
+++ js/detail-album.js
@@ -0,0 +1,76 @@
+window.addEventListener('load', function () {
+
+    // Declaramos variables globales para insertar info en HTML
+
+    let articulo = document.querySelector('main');
+
+    // Declaramos variables que vamos a usar globalmente para captar la informacion de la API
+
+    let url;
+    let album;
+    let genero;
+    let idGenero;
+    let artista;
+    let idArtista;
+    let fpub; // Fecha de publicación - formato AAAA/MM/DD
+    let stop = []; // Para el stop del bucle for
+    let canciones = []; // Array porque supongo que un album tiene varias
+    let idCanciones = []; // Array para almacenar cada ID
+
+    // Necesitamos captar el ID del albúm que se solicitó para mostrar la información requerida
+    // este debería llegar con sessionStorage
+
+    let idAlbumRecuperado = '188385452' //sessionStorage.getItem('idAlbum'); // debería llegar un string
+
+    //fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/${idAlbumRecuperado}`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            album = data.title;
+            console.log(`Nombre del album: ${album}`);
+            url = data.cover;
+            console.log(`Url de la imagen de portada ${url}`);
+            genero = data.genres.data[0].name;
+            console.log(`Género: ${genero}`);
+            idGenero = data.genres.data[0].id;
+            console.log(`ID del género: ${idGenero}`)
+            artista = data.contributors[0].name;
+            console.log(`Nombre del/los artistas ${artista}`);
+            idArtista = data.contributors[0].id;
+            console.log(`ID del género: ${idArtista}`)
+            fpub = data.release_date;
+            console.log(`Fecha de publicación: ${fpub}`);
+            stop = data.tracks.data;
+            for (let i = 0; i < stop.length; i++) {
+                canciones.push(stop[i].title)
+                idCanciones.push(stop[i].id)
+            }
+            console.log(`Nombre de la canción es: ${canciones}`)
+            console.log(`ID de la canción: ${idCanciones}`)
+
+            // ahora que tengo la info, la imprimo en el HTML
+            articulo.innerHTML = `
+                <h1>Detalle del álbum: ${album}</h1>
+                <article>
+                    <img src="${url}" alt="${album} - ${artista} - ${genero}">
+                    <a>${album}</a>
+                    <a href="detail-genre.html#${idGenero}">${genero}</a>
+                    <a href="detail-artist.html#${idArtista}">${artista}</a>
+                    <p>Fecha de publicación: ${fpub}</p>
+                    <ul></ul>
+                </article>
+            `
+
+            let listaCanciones = document.querySelector('main article ul');
+
+            for (let i = 0; i < stop.length; i++) {
+                listaCanciones.innerHTML += `
+                    <li><a href="detail-track.html#${idCanciones[i]}">${canciones[i]}</a></li>
+                `
+            }
+        })
+        .catch(function (error) {
+            console.log(`Este es el error: ${error}`)
+        })
+})
---
+++ js/menu-nav.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', function () {
+    console.log('la página cargó correctamente');
+
+    //función menu mobile
+    let navBotonOpen = document.querySelector('header button');
+    let navBotonClose = document.querySelector('#mobileNav button');
+    let mobileNav = document.querySelector('#mobileNav');
+
+    navBotonOpen.addEventListener('click', function () {
+        mobileNav.style.display = 'block';
+        navBotonOpen.style.display = 'none';
+        navBotonClose.addEventListener('click', function () {
+            mobileNav.style.display = 'none';
+            navBotonOpen.style.display = 'block';
+        })
+    })
+
+})
