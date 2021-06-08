030213c9b62a68184f947cae22fee55b987544de
Mati2108 <caloperaltaramosm@udesa.edu.ar>
2021-06-08 17:47:44 -0300

    fetch artistas y mini correciones

--- script/detailsong.js
+++ script/detailsong.js
@@ -1,11 +1,45 @@
 window.addEventListener('load', function() {
 
-
    // selecciono el elemento que desencadenará la accion
 const clickheart = document.querySelectorAll('.heart')
 
-
 const clickplay = document.querySelectorAll(".play")
+
+let url;
+let img;
+let cancion;
+let nombre;
+
+
+
+/*fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/27/top")
+.then (function (response) {
+   return response.json();
+})
+.then(function (datos) {
+   console.log(datos);
+for (let index = 0; index < datos.data.length; index++) {
+  
+  url = datos.data[index].link
+   console.log(url);
+
+  img = datos.data[index].album.cover_medium
+   console.log(img);
+
+  cancion = datos.data[index].preview
+   console.log(cancion);
+
+  nombre = datos.data[index].title
+   console.log(nombre);
+
+}
+
+})
+.catch (function (error) {
+   console.log('El error fue: ' + error);
+})*/
+
+
 
 for (let i = 0; i < clickheart.length; i++) {
    const element = clickheart[i];
