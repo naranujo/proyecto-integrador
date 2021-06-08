b529ee45a241314b547ee417cc0d005c7be4b73f
Mati2108 <caloperaltaramosm@udesa.edu.ar>
2021-06-08 12:06:34 -0300

    MouseOver + Click Arrgleado(toggle)

--- script/detailsong.js
+++ script/detailsong.js
@@ -1,13 +1,27 @@
 window.addEventListener('load', function(){
 
-let clickheart = document.querySelector('.heart')
 
-let clickplay = document.querySelector("#play")
+   // selecciono el elemento que desencadenará la accion
+const clickheart = document.querySelector('#heart')
+
+const clickplay = document.querySelector("#play")
 
 let clickHeWhi = document.querySelector(".heartConClick")
 
+
 clickheart.addEventListener('click', function () {
-    clickheart.classList.toggle(".heartConClick")
+// agrega/quita la clase OK
+clickheart.classList.toggle("ok");
+ alert("FUNCIONA")
+    console.log(clickheart);
+})
+
+
+clickplay.addEventListener('click', function () {
+  // agrega/quita la clase OK
+clickplay.classList.toggle("ok");
+alert("FUNCIONA")
+   console.log(clickplay);
 })
 
 
